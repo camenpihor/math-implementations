7f482dac0380189c50eb908c44705bd4133efdd7
Camen Piho <camen@getfreebird.com>
2019-05-18 00:01:22 -0700

    Update integration to work for partial derivatives

--- math_implementations/calculus/function.py
+++ math_implementations/calculus/function.py
@@ -8,20 +8,26 @@ class Function:
     Provides additional methods to aid differentiation and integration.
     """
 
-    e = 1e-2
+    e = 1e-4
 
-    def __init__(self, function_def, num_dims):
+    def __init__(self, function_def, num_inputs, output_dims=None):
         """Wrap functions to aid differeentiation and integration.
 
         Parameters
         ----------
         function_def : function
-            function from R^N -> R.
+            function from R^N -> R^M.
             The number of function arguments must equal its dimensionality.
-
+        input_dim : int
+            N in R^N -> R^M
+        output_dims : None | tuple
+            the dimensionality of the matrix returned by the function for each input. Defaults to
+            None, in the case of a function from R^N to R, since no matrix is returned for each
+            input.
         """
         self.fn = function_def
-        self.n_dims = num_dims
+        self.input_dim = num_inputs
+        self.output_dims = output_dims
 
     def __call__(self, *args):
         """Call function.
@@ -34,21 +40,17 @@ class Function:
         Returns
         -------
         Array
-            Array of dimension `self.n_dims`
+            Array of dimension `self.dims`
 
         """
-        self.check_dimensionality(args)
         f_args = [Array(arg) if isinstance(arg, list) else Array([arg]) for arg in args]
-        return Array(self.fn(*f_args))
-
-    def check_dimensionality(self, args):
-        """Ensure that the dimensionality of the input is equal to that of `self.f`."""
-        if len(args) != self.n_dims:
-            raise DimensionMismatchExpcetion(f"Invalid number of arguments! Must be {self.n_dims}")
+        result = Array(self.fn(*f_args))
+        if result.shape == (1,):
+            result = result[0]
+        return result
 
     def __differentiate(self, *data):
         data = list(data)
-        self.check_dimensionality(data)
 
         if not isinstance(data, list):
             data = [data]
@@ -65,15 +67,26 @@ class Function:
 
         return partials
 
-    def __integrate_left(self, x):
-        # TODO rework for multiple dimensions
+    def __integrate_left(self, lower_bound, upper_bound):
+        "This doesn't work in the case of integrating f: x^2 + y^2. Think about what to do to get the reimann sum of that thing"
+        lower_bound = lower_bound[0]
+        upper_bound = upper_bound[0]
+
         cum_sum = 0
         integral = []
-        for element in x:
-            at_point = self(element) * self.e
-            integral.append(at_point + cum_sum)
-            cum_sum += at_point
-        return Array(integral)
+        current_point = Array([lower_bound] * self.input_dim)
+        while current_point[0] < upper_bound:
+            print(current_point)
+            at_point = self(*current_point) * self.e
+
+            if not isinstance(at_point, list):
+                cum_sum += at_point
+            else:
+                cum_sum += sum(at_point)
+
+            integral.append(cum_sum)
+            current_point = current_point + self.e
+        return integral
 
     @property
     def differentiate(self):
@@ -85,7 +98,17 @@ class Function:
             f -> f'(x)
 
         """
-        return Function(self.__differentiate, self.n_dims)
+        output_dims = self.output_dims
+
+        if output_dims is None:
+            output_dims = (self.input_dim,)
+
+        else:
+            output_dims += (self.input_dim,)
+
+        return Function(
+            function_def=self.__differentiate, num_inputs=self.input_dim, output_dims=output_dims
+        )
 
     @property
     def integrate(self):
@@ -97,7 +120,7 @@ class Function:
             f -> F(x)
 
         """
-        return Function(self.__integrate_left, self.n_dims)
+        return Function(self.__integrate_left, num_inputs=self.input_dim, output_dims=None)
 
 
 class DimensionMismatchExpcetion(Exception):
